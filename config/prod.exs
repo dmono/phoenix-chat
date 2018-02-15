8a4b7c8a4964e69cfdcab94d498eab1d6f81b14c
Terry Lee <terryleedev@gmail.com>
2018-02-15 14:02:37 -0800

    change check_origin setting

--- config/prod.exs
+++ config/prod.exs
@@ -16,6 +16,7 @@ use Mix.Config
 config :hello, HelloWeb.Endpoint,
   load_from_system_env: true,
   url: [scheme: "https", host: "https://morning-hollows-67476.herokuapp.com/", port: 443],
+  check_origin: false,
   cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
